f505248ec6dba89c5a44addd1de1a4e9ff6369e5
andrelrocha <andrerocha0911@gmail.com>
2023-07-12 14:51:04 -0300

    feat: middleware de erros de rota

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
 
-import { db, client } from "./db/dbConnect";
+import { db } from "./db/dbConnect";
+import { errorHandle400 } from "./middleware/errors/ErrorHandle400";
+import { errorHandle404 } from "./middleware/errors/ErrorHandle404";
+
 
 db.on("error", console.log.bind(console, "Database connection error!"));
 db.on("connect", () => console.log("Database connected!"));
 
 const app = express();
 app.use(express.json());  
+
+app.use(errorHandle400);
+app.use(errorHandle404);
 
 app.get("/teste", (req, res) => {
     res.status(200).send({message: "Hello World!"});
